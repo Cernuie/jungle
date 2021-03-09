6242b3730d8a58c2258cb7f3bf29b81210939219
Alexander Nguyen <cernvii@gmail.com>
2021-03-09 06:45:34 +0000

    fix user validation for email (uniqueness instead of confirmation) and rest of user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, confirmation: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, length: {minimum: 4}
 
   def self.authenticate_with_credentials(email, password)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,44 @@ RSpec.describe User, type: :model do
       @user = User.new( first_name: "Test", last_name: "Name", email: "Test@Test.com", password: "12345", password_confirmation: "12345")
       expect(@user).to be_valid
     end
+
+    it 'is invalid without a first name' do
+      @user = User.new( first_name: nil, last_name: "Name", email: "Test@Test.com", password: "12345", password_confirmation: "12345")
+      expect(@user).to be_invalid
+    end
+
+    it 'is invalid without a last name' do
+      @user = User.new( first_name: "Test", last_name: nil, email: "Test@Test.com", password: "12345", password_confirmation: "12345")
+      expect(@user).to be_invalid
+    end
+
+    it 'is invalid without an email' do
+      @user = User.new( first_name: "Test", last_name: "Name", email: nil, password: "12345", password_confirmation: "12345")
+      expect(@user).to be_invalid
+    end
+
+    it 'is invalid if there is no password ' do
+      @user = User.new( first_name: "Test", last_name: "Name", email: "Test@Test.com", password: nil, password_confirmation: "12345")
+      expect(@user).to be_invalid
+    end
+
+    it 'is invalid if the passwords do not match' do
+      @user = User.new( first_name: "Test", last_name: "Name", email: "Test@Test.com", password: "123456", password_confirmation: "1234567")
+      expect(@user).to be_invalid
+    end
+
+    it 'is invalid if the password is less than 4 characters ' do
+      @user = User.new( first_name: "Test", last_name: "Name", email: "Test@Test.com", password: "12", password_confirmation: "12")
+      expect(@user).to be_invalid
+    end
+
+    it 'is invalid if there is already a user for that email ' do
+      @user = User.new( first_name: "Test", last_name: "Name", email: "Test@Test.com", password: "12345", password_confirmation: "12345")
+      @user.save
+      @user2 = User.new( first_name: "Test", last_name: "Name", email: "Test@Test.com", password: "12345", password_confirmation: "12345")
+      expect(@user2).to be_invalid
+    end
+
   end
 
   describe '.authenticate_with_credentials' do
